b6099d59f1ee8bcf4534d633e9304ecc2dd3b6ff
Nguyen-Cong-Nghiem <kukull.ncn@gmail.com>
2018-06-16 08:13:23 +0700

    Add file TinhDiemTrungBinh

---
+++ src/TinhDiemTrungBinh.java
@@ -0,0 +1,18 @@
+
+public class TinhDiemTrungBinh {
+    public static void main(String[] args) {
+        int a=10, b=20,c=40;
+        int diemtrungbinh;
+        diemtrungbinh=(a+b+c)/3;
+        System.out.println("Diem trung binh= "+diemtrungbinh);
+
+    }
+
+
+
+
+
+
+
+
+}
